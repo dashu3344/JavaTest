7f38cb3c783267e3f693ec73e981bb448732796a
lxs <dashu3344@163.com>
2017-11-21 14:23:56 +0800

    Java项目初始化

---
+++ src/com/lxs/java/Test.java
@@ -0,0 +1,17 @@
+package com.lxs.java;
+
+/**
+ * Name: Test
+ * Author: lxs
+ * Email:
+ * Comment: //TODO
+ * Date: 2017-11-21 14:20
+ */
+public class Test {
+	private String str = "String";
+
+	public static void main(String[] args) {
+
+		System.out.println("Test");
+	}
+}
